e2b7113a7c1e0c67d5f02c83b6b6ba4c7c587fa4
vanshika54 <96998102+vanshika54@users.noreply.github.com>
2022-01-27 15:08:01 +0530

    Create 11.cpp

---
+++ Microsoft/11.cpp
@@ -0,0 +1,41 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+string decToBinary(int n){
+    string s;
+    for (int i = 31; i >= 0; i--) {
+        int k = n >> i;
+        if (k & 1)
+            s.push_back('1');
+        else
+            s.push_back('0');
+    }
+    for(int i=0;i<32;i++){
+        if(s[i] == '1') 
+            return s.substr(i);
+    }
+}
+vector<string> generate(int N){
+    vector<string> ans;
+	for(int i=1;i<=N;i++){
+	    string temp = decToBinary(i);
+	    ans.push_back(temp);
+	}
+	return ans;
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<string> ans = generate(n);
+		for(auto it:ans) cout<<it<<" ";
+		cout<<endl;
+	}
+	return 0;
+}
+
