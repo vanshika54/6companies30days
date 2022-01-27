e4ae1c6f2519a32a6f49280bf5778da76447acee
vanshika54 <96998102+vanshika54@users.noreply.github.com>
2022-01-27 14:47:43 +0530

    Create 12.cpp

---
+++ Goldsman sachs/12.cpp
@@ -0,0 +1,32 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+ 
+long long squaresInChessBoard(long long N) {
+       long long sum=0;
+       long long i;
+       for(i=1;i<=N;i++)
+       {
+           sum=sum+i*i;
+       }
+       return sum;
+   }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}
+
