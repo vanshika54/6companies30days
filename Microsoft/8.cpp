c9eed6cf2eecdab35e5153605dea91d53e4c505a
vanshika54 <96998102+vanshika54@users.noreply.github.com>
2022-01-27 15:11:29 +0530

    Create 8.cpp

---
+++ Microsoft/8.cpp
@@ -0,0 +1,141 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node* nextRight;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->nextRight = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+    Node* root = newNode(stoi(ip[0]));
+    queue<Node*> queue;
+    queue.push(root);
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        string currVal = ip[i];
+        if(currVal != "N") {
+                
+            currNode->left = newNode(stoi(currVal));
+                
+            queue.push(currNode->left);
+        }
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+        if(currVal != "N") {
+            currNode->right = newNode(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void connect(struct Node *p);
+void printSpecial(Node *root)
+{
+   if (root == NULL)
+     return;
+
+   Node* next_root=NULL;
+
+   while (root != NULL)
+   {
+      cout<< root->data<<" ";
+
+      if( root->left && (!next_root) )
+        next_root = root->left;
+      else if( root->right && (!next_root)  )
+        next_root = root->right;
+
+      root = root->nextRight;
+   }
+   
+   printSpecial(next_root);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+class Solution
+{
+    public:
+    void connect(Node *root)
+    {
+       queue<Node*>q;
+        q.push(root);
+        while(!q.empty()){
+          int c=q.size();
+          for(int i=0;i<c;i++){
+              Node* n=q.front();
+              q.pop();
+              if(n->left)
+              q.push(n->left);
+              if(n->right)
+              q.push(n->right);
+              if(i+1==c)
+              n->nextRight=NULL;
+              else
+              n->nextRight=(q.front());
+                              }
+                          }
+    }    
+      
+};
+
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     string s;
+     getline(cin, s);
+     Node* root = buildTree(s);
+        
+    Solution obj;
+     obj.connect(root);
+     printSpecial(root);
+     cout<<endl;
+     inorder(root);
+     cout<<endl;
+  }
+  return 0;
+}
+
+
+
