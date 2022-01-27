3c96698e8cfd4ea33d3be2b6a7987ce2a9b05c22
vanshika54 <96998102+vanshika54@users.noreply.github.com>
2022-01-27 15:15:18 +0530

    Create 2.cpp

---
+++ Microsoft/2.cpp
@@ -0,0 +1,57 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+	bool isPossible(int N, vector<pair<int, int> >& prerequisites) {
+	    vector<int> adj[N];
+	    for(auto& it: prerequisites) {
+	        adj[it.first].push_back(it.second);
+	    }
+	    int cnt = 0;
+	    vector<int> indeg(N, 0);
+	    for(int i=0; i<N; i++) {
+	        for(auto& it: adj[i]) {
+	            indeg[it]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0; i<N; i++) {
+	        if(indeg[i]==0) q.push(i);
+	    }
+	    while(!q.empty()) {
+	        int node = q.front();
+	        q.pop();
+	        cnt++;
+	        for(auto& it: adj[node]) {
+	            indeg[it]--;
+	            if(indeg[it]==0) q.push(it);
+	        }
+	    }
+	    return (cnt==N);
+	}
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        Solution ob;
+        if (ob.isPossible(N, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
