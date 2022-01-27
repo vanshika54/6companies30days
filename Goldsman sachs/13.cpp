978fa8c54eee91527a410fbb90b5233834ad2990
vanshika54 <96998102+vanshika54@users.noreply.github.com>
2022-01-27 14:48:41 +0530

    Create 13.cpp

---
+++ Goldsman sachs/13.cpp
@@ -0,0 +1,56 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+   string decodedString(string s){
+       int currNo=0;
+       string currString="";
+       int prevNo;
+       string prevString;
+       stack<string>stringstack;
+       stack<int>numstack;
+       for(int i=0;i<s.size();i++)
+       {
+           if(isdigit(s[i])) currNo = currNo*10 + (s[i]-'0');
+           if(isalpha(s[i])) currString += s[i];
+           if(s[i] == '[')
+           {
+               stringstack.push(currString);
+               numstack.push(currNo);
+               currNo=0;
+               currString="";
+           }
+           if(s[i] == ']')
+           {
+               prevNo = numstack.top();
+               numstack.pop();
+               prevString = stringstack.top();
+               stringstack.pop();
+               
+               while(prevNo--)
+               {
+                  prevString += currString;
+               }
+               currString = prevString;
+           }
+       }
+       return currString;
+       
+       
+   }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        
+        Solution ob;
+        cout<<ob.decodedString(s)<<"\n";
+    }
+    return 0;
+}
+
