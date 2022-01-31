96aa119686d40fe72f6f21ad3d5bf5cbb387eecb
vanshika54 <96998102+vanshika54@users.noreply.github.com>
2022-01-31 12:14:40 +0530

    Create 13.cpp

---
+++ Walmart/13.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(), nums.end(), [](const string& a, const string& b) {
+            if (a.size() == b.size()) {
+                return a > b;
+            }
+            return a.size() > b.size();
+        });
+        return nums[k - 1];
+    }
+};
+
+
+
